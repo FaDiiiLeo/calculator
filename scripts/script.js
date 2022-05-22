00e6c0120da611f340657b357605e387b6453a90
Fahad Akram <45397440+FaDiiiLeo@users.noreply.github.com>
2022-05-23 02:54:42 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,15 @@ let themeValue = 1;
 function lastSelectedTheme(){
     themeValue = localStorage.getItem('selectedTheme');
     if(themeValue === '1'){
-        themeStyleSheet.href = '/styles/themes/theme-1.css';
+        themeStyleSheet.href = 'styles/themes/theme-1.css';
         themeSelector.value = localStorage.getItem('toggleValue');
     }
     else if(themeValue === '2'){
-        themeStyleSheet.href = '/styles/themes/theme-2.css';
+        themeStyleSheet.href = 'styles/themes/theme-2.css';
         themeSelector.value = localStorage.getItem('toggleValue');
     }
     else if(themeValue === '3'){
-        themeStyleSheet.href = '/styles/themes/theme-3.css';
+        themeStyleSheet.href = 'styles/themes/theme-3.css';
         themeSelector.value = localStorage.getItem('toggleValue');
     }
 }
@@ -23,13 +23,13 @@ function selectTheme(){
     themeSelector.addEventListener('input',function(){
         themeValue = themeSelector.value;
         if(themeValue === '1'){
-            themeStyleSheet.href = '/styles/themes/theme-1.css';
+            themeStyleSheet.href = 'styles/themes/theme-1.css';
         }
         else if(themeValue === '2'){
-            themeStyleSheet.href = '/styles/themes/theme-2.css';
+            themeStyleSheet.href = 'styles/themes/theme-2.css';
         }
         else if(themeValue === '3'){
-            themeStyleSheet.href = '/styles/themes/theme-3.css';
+            themeStyleSheet.href = 'styles/themes/theme-3.css';
         }
         if (typeof(Storage) !== 'undefined') {
             localStorage.setItem('selectedTheme', themeValue);
